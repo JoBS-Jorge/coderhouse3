c9b442df7915805421cc78fb23fa96140498b329
Jorge Braz de Souza Filho <jorge.not@gmail.com>
2023-06-06 13:14:57 -0300

    Initial commit

---
+++ loopContagem.js
@@ -0,0 +1,9 @@
+let numeroFinal = prompt("Digite um numero para contagem na tela:")
+let todosOsNumeros = []
+
+for (i = 1 ; i <= numeroFinal ; i++){
+    console.log(i)
+    todosOsNumeros.push(i)
+}
+
+console.log(todosOsNumeros)
